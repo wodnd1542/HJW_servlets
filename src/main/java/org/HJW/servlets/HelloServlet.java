ed97e8dc2b5e96b8bef51996486192313acc2dab
HJW-jdbc <wodnd1542@naver.com>
2019-04-19 15:43:01 +0900

    4/19수업내용

--- src/main/java/org/HJW/servlets/HelloServlet.java
+++ src/main/java/org/HJW/servlets/HelloServlet.java
@@ -3,6 +3,7 @@ package org.HJW.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * 
  * @author Jacob
  */
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
 	@Override
